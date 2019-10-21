4d9a8c4571c260e813b7bae54ca7146002926536
dinh duy <dinhvanduy7895@gmail.com>
2019-10-22 00:42:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const webpush = require('web-push')
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+const port = 4000
+app.get('/', (req, res) => res.send('Hello World!'))
+const dummyDb = { subscription: null } //dummy in memory store
+const saveToDatabase = async subscription => {
+  // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
+  // Here you should be writing your db logic to save it.
+  dummyDb.subscription = subscription
+}
+// The new /save-subscription endpoint
+app.post('/save-subscription', async (req, res) => {
+  const subscription = req.body
+  await saveToDatabase(subscription) //Method to save the subscription to Database
+  res.json({ message: 'success' })
+})
+const vapidKeys = {
+  publicKey:
+    'BLp9v6cBemTH8--9SXE_g29WcKDtgAz_pd1oWd6OalLMl09jcBsCQCJaUCoq2RnbRQwhWnPAZFdSbhI2IiEduag',
+  privateKey: 'N1T44St4JUrtO8JR4kAZ8h4XKT-il53DLuhnfeT4im8',
+}
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+  'mailto:myuserid@email.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+  webpush.sendNotification(subscription, dataToSend)
+}
+//route to test send notification
+app.get('/send-notification', (req, res) => {
+  const subscription = dummyDb.subscription //get subscription from your databse here.
+  const message = 'Hello World'
+  sendNotification(subscription, message)
+  res.json({ message: 'message sent' })
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
